8cc03675b7adc01bcd48c5167980be482b6f6803
chenxuguo <806030341@qq.com>
2023-09-28 15:37:14 +0800

    First commit.

---
+++ app/src/main/java/com/example/coolweather/db/City.java
@@ -0,0 +1,29 @@
+package com.example.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by chenxuguo on 23-9-28.
+ */
+
+public class City extends DataSupport {
+    private int id;
+
+    private String cityName;
+
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName){
+        this.cityName = cityName;
+    }
+
+    public int 
+}
