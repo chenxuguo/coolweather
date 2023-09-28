aaf874ced7995758f37c59cc14c67b2c3d4dba92
chenxuguo <806030341@qq.com>
2023-09-28 15:55:45 +0800

    加入创建数据库和表的各项配置

--- app/src/main/java/com/example/coolweather/db/City.java
+++ app/src/main/java/com/example/coolweather/db/City.java
@@ -13,6 +13,8 @@ public class City extends DataSupport {
 
     private int provinceId;
 
+    private int cityCode;
+
     public int getId() {
         return id;
     }
@@ -25,5 +27,19 @@ public class City extends DataSupport {
         this.cityName = cityName;
     }
 
-    public int 
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode){
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
 }
